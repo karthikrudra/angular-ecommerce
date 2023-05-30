baa5564915c995714283e91a6161ca800e23da00
karthikrudra <129721023+karthikrudra@users.noreply.github.com>
2023-05-30 12:19:44 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,57 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
+// import { environment } from './environment.prod';
 export const environment = {
-  production: false
-};
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+  production: false,
+  userurl:'http://localhost:9051/',//for user service
+  adminurl:'http://localhost:9054/', //for admin service
+  carturl:'http://locaalhost:9053/',
+  sellerurl:'http://localhost:9052/',
+
+  produserurl:'http://34.122.139.85:9051', //for inventory,
+  prodadminurl:'http://34.122.139.85:9054', //for authentication service
+  prodcarturl:'http://34.122.139.85:9053',
+  prodsellerurl:'http://34.122.139.85:9052',
+
+
+
+  getApiUrl(data:string) {
+
+    if(environment.production){
+      if(data==='user'){
+        return environment.produserurl;
+      }
+      else if(data=='admin'){
+        return environment.prodadminurl;
+      }
+      else if(data=='cart'){
+        return environment.prodcarturl;
+      }
+      else if(data==='seller'){
+        return environment.sellerurl;
+      }
+      else{
+        return;
+      }
+    }
+    else{
+
+      if(data==='user'){
+        return environment.userurl;
+      }
+      else if(data==='admin'){
+        return environment.adminurl;
+      }
+      else if(data==='cart'){
+        return environment.carturl;
+      }
+      else if(data==='seller'){
+        console.log("hitting url");
+        return environment.sellerurl;
+      }
+      else{
+        return;
+      }
+    }
+  }
+
+};
